2b057cd5d0b12dd048233ca9f112bbfb3a6eac6d
KvnSG <kevinsuntaxi@hotmail.com>
2021-09-12 14:59:36 -0500

    Update auditoria 4/??

--- proyecto/php/editar_admin_actualizar.php
+++ proyecto/php/editar_admin_actualizar.php
@@ -11,12 +11,16 @@ $direccion = $_POST['direccion'];
 $actualizar = "UPDATE administrador SET ADM_NOMBRE='$nombre', ADM_APELLIDO='$apellido', ADM_TELEFONO='$telefono', ADM_FNACIMIENTO='$fnac', ADM_DIRECCION='$direccion'  WHERE ADM_CODIGO='$ide' ";
 $result2 = mysqli_query( $conn, $actualizar );
 $myIP=getRealIP();
+
+session_start();
+$user_session = $_SESSION['user'];
+
 if($result2){
 	echo '<script>
 			alert("Los datos se han actualizado correctamente");
 			window.location="editar_admin.php";
 		</script>';
-	$auditoria = mysqli_query( $conn,"INSERT INTO `auditoria`(`USU_CODIGO`, `AUD_IP`, `AUD_EVENTO`, `AUD_HORA`, `AUD_FECHA`) VALUES (1,'$myIP','Actualizo Administrador',curTime(),CURDATE())");
+	$auditoria = mysqli_query( $conn,"INSERT INTO `auditoria`(`USU_CODIGO`, `AUD_IP`, `AUD_EVENTO`, `AUD_HORA`, `AUD_FECHA`) VALUES ($user_session,'$myIP','Actualizo Administrador',curTime(),CURDATE())");
 } else{
 	echo '<script>
 			alert("Hubo un error al guardar");
